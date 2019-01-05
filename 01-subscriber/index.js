6a8391e6d52a2b7dc3cc71301423c452826afea0
chaomian123 <41313295+chaomian123@users.noreply.github.com>
2019-01-05 20:56:31 +0800

    rxjs-learning
    balabala

--- 01-subscriber/index.js
+++ 01-subscriber/index.js
@@ -1,17 +1,15 @@
-import { from } from "rxjs"
+import { Observable } from "rxjs/Observable";
 
-const observable$ = from([1, 2, 3, 4, 5])
+const onSubscrible = observer => {
+  observer.next(1);
+  observer.error("Something wrong");
+  observer.complete();
+};
+const source$ = new Observable(onSubscrible);
+const theObserver = {
+  next: item => console.log(item),
+  error: err => console.log(err),
+  complete: () => console.log("no more data")
+};
 
-const subscriber = {
-  next: value => {
-    console.log(value)
-  },
-  complete: () => {
-    console.log("done")
-  },
-  error: value => {
-    console.log(value)
-  }
-}
-
-observable$.subscribe(subscriber)
+source$.subscribe(theObserver);
